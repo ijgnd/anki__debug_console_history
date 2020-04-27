2578335866b455ce062e00334eaadfb211c2e0d9
ijgnd <fooijgnd@barijgnd>
2020-04-27 11:19:06 +0200

    more tweaks

--- src/__init__.py
+++ src/__init__.py
@@ -27,13 +27,13 @@ This add-on uses the file fuzzy_panel.py which has this copyright and permission
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
+    This file is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this file.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import os
